528d2f64f666cfdd35b61796b83fb80811546cf6
Pranav <pranavpk0@outlook.com>
2021-10-14 07:08:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+let backspace = function () {
+  let inputText = document.getElementById("display-box").value;
+  document.getElementById("display-box").value = inputText.substring(
+    0,
+    inputText.length - 1
+  );
+};
+
+const toggleSwitch = document.querySelector(
+  '.theme-switch input[type="checkbox"]'
+);
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+  }
+}
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+  }
+}
+
+toggleSwitch.addEventListener("change", switchTheme, false);
